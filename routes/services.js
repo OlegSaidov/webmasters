f4ed28e55a965f200b0ad13bca61c98ad2144b79
Oleg Saidov <oleg_saidov@yahoo.com>
2018-10-31 17:51:08 -0400

    Tried upload webmasters website on Heroku

---
+++ routes/services.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var fs = require("fs")
+var filedata
+fs.readFile("json/services.json","utf8", function(err, data){
+  if(err) throw Error;
+  else {
+  filedata = JSON.parse(data)
+  }
+});
+
+
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('services', {
+    title:'Services'
+    serv: filedata});
+});
+
+module.exports = router;
