7996bbd018b0c4b10e05c81c2e37a2b1c9066311
Oleg Saidov <oleg_saidov@yahoo.com>
2018-11-02 21:58:38 -0400

    Fixed the webApp and made it ready for contact page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require("fs")
+var filedata
+fs.readFile("json/services.json","utf8", function(err, data){
+  if(err) throw Error;
+  else {
+  filedata = JSON.parse(data)
+  }
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Oleg' });
+  res.render('index', { title: 'Webmasters',
+                        serv: filedata
+ });
 });
 
 module.exports = router;
